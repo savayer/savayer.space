e69658cf70f90e296c51682746dc33b19aa7d51d
savayer <sava0094@gmail.com>
2018-09-01 15:50:50 +0700

    first steps of creating layput in vue template. In future i want to make mini blog

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="wrapper">
+    <div class="background-slides">
+      <img src="./assets/img/slide-1.jpg" alt="">  
+    </div> 
+    <div class="overlay skew">
+      <div class="overlay__stars"></div>
+    </div>
+    <section id="block-left">
+      <div class="content">
+        <h1>Hi, there!</h1>
+        <p>I'm Junior frontend-developer. It's my homepage, where i write some posts for myself about developing, english language, etc.</p>
+        <button class="btn btn-white">
+          More Informations
+        </button>
+      </div>
+      
+    </section>
+    <section id="block-right">
+
+    </section>
+
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'app',
+
+}
+</script>
+
+<style lang="scss" src="./assets/sass/main.scss"></style>
