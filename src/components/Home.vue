58b5838be0cb080168302614a9426e4cbdb3af46
Chingis Savinov <sava0094@gmail.com>
2018-09-15 16:15:40 +0700

    for homepage finished editing html and css

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -8,10 +8,18 @@
       <div class="content">
         <h1>Hi, there!</h1>
         <p>I'm Junior frontend-developer. I want to grow as a developer. I like to create web-sites, write code in JavaScript. Skills: HTML, CSS, JavaScript (JQuery, Vuejs), basic knowledge of PHP, Laravel</p>
-        <button class="btn btn-white" @click="show = !show">
-          {{ changeButtonText }}
-        </button>
-        <br><br>
+        <ul class="ul_buttons">
+          <li>
+            <a class="btn btn-white" @click="show = !show">
+              {{ changeButtonText }}
+            </a>    
+          </li>
+          <li>
+            <router-link to="posts/" class="btn btn-green">
+              Blog
+            </router-link>
+          </li>
+        </ul>              
         <div class="social">
           <a href="http://t.me/savayer">
             <span class="icon-telegram"></span>
@@ -66,6 +74,12 @@
             <div class="introtext"> {{ post.introtext }} </div>
           </div>
         </div>
+        <button class="btn btn-green-content btn-center">
+          <router-link to="posts/">
+            Все посты
+          </router-link>
+        </button>
+        
       </section>
     </transition>
   </div>
@@ -86,8 +100,28 @@ export default {
           content: `<h1>it's my first experince with SPA</h1>
 
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae delectus architecto a cum. Eligendi, a id! Aliquam quis cumque beatae doloremque, ipsa, quaerat sunt voluptatum iure hic sequi, provident adipisci.</p>`
+        },
+        {
+          id: 2,
+          img: "Vue.png",
+          slug: "first-try-create-spa",
+          pagetitle: "Первая попытка осознания архитектуры SPA",
+          introtext: "Пробуем-с создать SPA с помощью Vuejs...",
+          content: `<h1>it's my first experince with SPA</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae delectus architecto a cum. Eligendi, a id! Aliquam quis cumque beatae doloremque, ipsa, quaerat sunt voluptatum iure hic sequi, provident adipisci.</p>`
+        },
+        {
+          id: 3,
+          img: "Vue.png",
+          slug: "first-try-create-spa",
+          pagetitle: "Первая попытка осознания архитектуры SPA",
+          introtext: "Пробуем-с создать SPA с помощью Vuejs...",
+          content: `<h1>it's my first experince with SPA</h1>
+
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Vitae delectus architecto a cum. Eligendi, a id! Aliquam quis cumque beatae doloremque, ipsa, quaerat sunt voluptatum iure hic sequi, provident adipisci.</p>`
         }
-        /* { id: 2, slug: 'second-post', pagetitle: 'test 2', introtext: 'intro 2', content: 'no comments' }, */
+        
       ]
     };
   },
